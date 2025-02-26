a1d76bb56c9cb68350bcf5ca92d3ba24f95857ad
Madson1999 <madson_michel@hotmail.com>
2025-02-26 14:41:38 -0400

    Atualização main

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, make_response
-<<<<<<< HEAD
 from dados.dados_motos import motos
 from dados.user import usuarios
 from dados.taxa import taxas
 
-#inicio
 app = Flask(__name__)
 app.secret_key = 'segredo'  
 
@@ -12,19 +10,6 @@ formas_pagamentos = {
     "a_vista": "À vista",
     "financiado": "Financiado",
     "cartao": "Cartão de Crédito"
-=======
-from motos_novas.dados_motos import motos
-from users.user import usuarios
-
-
-app = Flask(__name__)
-app.secret_key = 'segredo'  
-
-
-formas_pagamentos = {
-    "a_vista": "À vista",
-    "financiado": "Financiado"
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
 }
 
 bancos = {
@@ -33,18 +18,11 @@ bancos = {
     "santander": "Santander",
     "bradesco": "Bradesco",
     "c6bank": "C6Bank",
-<<<<<<< HEAD
-=======
-    "consorcio": "Consórcio",
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
     "cartao_credito": "Cartão de Crédito",
     "outros": "Outros"
 }
 
-<<<<<<< HEAD
 # Outras informações
-=======
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
 banco_retorno = {
     "zero": "R0",
     "um": "R1",
@@ -57,7 +35,6 @@ filiais = {
     "Cachoeirinha": "Cachoeirinha",
     "Compensa": "Compensa",
     "Cidade Nova": "Cidade Nova",
-<<<<<<< HEAD
     "Grande Circular": "Grande Circular",
     "Max Teixeira": "Max Teixeira",
     "Humaita": "Humaitá",
@@ -72,17 +49,6 @@ origem_moto = {
     "Capital": "Capital",
     "Interior": "Interior"
 }
-=======
-    "Max Teixeira": "Max Teixeira",
-    "humaita": "Humaitá",
-    "itacoatiara": "Itacoatiara",
-    "iranduba": "Iranduba",
-    "manacapuru": "Manacapuru",
-    "coari": "Coari",
-    "tefe": "Tefé",
-}
-
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
 
 @app.route('/')
 def home():
@@ -93,7 +59,6 @@ def login():
     login = request.form.get('login')
     senha = request.form.get('password')
 
-<<<<<<< HEAD
     if login in usuarios and usuarios[login] == senha:
         response = make_response(redirect(url_for('segmentos')))
         response.set_cookie('usuario_logado', login)
@@ -110,21 +75,6 @@ def segmentos():
     return render_template('segmento.html', usuario=usuario_logado.replace('_', ' ').title())
 
 # Rota para o painel
-=======
-    # Valida o login e senha
-    if login in usuarios and usuarios[login] == senha:
-        # Cria a resposta para o painel
-        response = make_response(redirect(url_for('painel')))
-        # Define o cookie com o nome do usuário logado
-        response.set_cookie('usuario_logado', login)
-        return response
-    else:
-        # Se houver erro, usa flash para armazenar a mensagem de erro
-        flash("Login ou senha incorretos")
-        # Redireciona de volta para a página de login (home)
-        return redirect(url_for('home'))
-
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
 @app.route('/painel', methods=['GET', 'POST'])
 def painel():
     usuario_logado = request.cookies.get('usuario_logado')
@@ -132,7 +82,6 @@ def painel():
     if not usuario_logado:
         return redirect(url_for('home'))
 
-<<<<<<< HEAD
     return render_template('painel.html', usuario=usuario_logado.replace('_', ' ').title(),
                            motos_yamaha=motos, formas_pagamentos=formas_pagamentos,
                            bancos=bancos, filiais=filiais, banco_retorno=banco_retorno, origem_moto=origem_moto, taxas=taxas)
@@ -140,16 +89,6 @@ def painel():
 @app.route('/dados_moto/<nome_moto>', methods=['GET'])
 def obter_dados_moto(nome_moto):
     nome_moto = nome_moto.replace('%20', ' ')
-=======
-    # Renderiza a página normalmente para a requisição GET
-    return render_template('painel.html', usuario=usuario_logado.replace('_', ' ').title(),
-                           motos_yamaha=motos, formas_pagamentos=formas_pagamentos,
-                           bancos=bancos, filiais=filiais, banco_retorno=banco_retorno)
-
-@app.route('/dados_moto/<nome_moto>', methods=['GET'])
-def obter_dados_moto(nome_moto):
-    nome_moto = nome_moto.replace('%20', ' ')  # Corrige o nome da moto se houver espaço
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
     if nome_moto in motos:
         dados_moto = motos[nome_moto]
         return jsonify({
@@ -162,22 +101,13 @@ def obter_dados_moto(nome_moto):
     else:
         return jsonify({'error': 'Moto não encontrada'}), 404
 
-<<<<<<< HEAD
 @app.route('/motos', methods=['GET'])
 def motos_painel():
     return redirect(url_for('painel'))
-=======
-
-
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
 
 @app.route('/logout')
 def logout():
     response = make_response(redirect(url_for('home')))
-<<<<<<< HEAD
-=======
-    # Exclui o cookie do usuário logado
->>>>>>> 0046c15d9255fa3b6ab3517b7c9616b051a1cbdc
     response.delete_cookie('usuario_logado')
     return response
 
