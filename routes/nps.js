e8e02f2d1ae1bd5d1f8abf1137043839f9a3645f
Luiz Miguel <luizmiguel@MacBook-Pro-de-Luiz.local>
2025-09-11 11:55:04 -0400

    add para mes 9

--- routes/nps.js
+++ routes/nps.js
@@ -8,7 +8,7 @@ async function atualizarNPS() {
   await connection.query(`TRUNCATE TABLE nps`);
 
   // SELECIONAR A DATA NA QUAL A PONTUAÇÃO IRÁ VALER
-  const mesReferente = '2025-08';
+  const mesReferente = '2025-09';
 
   const [resultados] = await connection.query(`
   SELECT 
