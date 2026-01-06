b26b04d22b301b777e9f0cd56e00b3b313a4d281
Luan Cruz <luanncruzz20@gmail.com>
2026-01-05 21:54:01 -0400

    feat modificações nos templates motos.js, servres.js e emplacamentos.js

--- services/emplacamento.js
+++ services/emplacamento.js
@@ -1,10 +1,10 @@
 // PORCENTAGEM DA EQUAÇAÕ DE EMPLACAMENTO, VARIA POR MÊS
 
 const emplacamento = {
-  "janeiro": 0.03, "fevereiro": 0.0275, "março": 0.025,
-  "abril": 0.0225, "maio": 0.02, "junho": 0.0175,
-  "julho": 0.015, "agosto": 0.0125, "setembro": 0.01,
-  "outubro": 0.0075, "novembro": 0.005, "dezembro": 0.0025
+  "janeiro": 0.015, "fevereiro": 0.0137, "março": 0.0125,
+  "abril": 0.0112, "maio": 0.01, "junho": 0.0087,
+  "julho": 0.0075, "agosto": 0.0062, "setembro": 0.005,
+  "outubro": 0.0037, "novembro": 0.025, "dezembro": 0.0012
 }
 
 module.exports = emplacamento;
